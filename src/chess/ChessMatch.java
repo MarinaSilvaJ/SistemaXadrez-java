4e832f99a6f760858a9602d78f7d02ba92d5c8c5
Marina Silva <marinasilva_93@hotmail.com>
2020-03-09 03:36:12 -0300

    Movimento Especial - Roque

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -105,6 +105,24 @@ public class ChessMatch {
 			capturedPieces.add(capturedPiece);
 		}
 		
+		// Implementando movimento da peca Torre no Roque Pequeno
+		if (p instanceof King && target.getColumn() == source.getColumn() + 2) { //Se peca que esta sendo movimentada for Rei e e posicao destino for igual posicao origem + 2, movimentar a peca Torre para posicao do movimento Roque Pequeno
+			Position sourceT = new Position(source.getRow(), source.getColumn() + 3);
+			Position targetT = new Position(source.getRow(), source.getColumn() + 1);
+			ChessPiece rook = (ChessPiece)board.removePiece(sourceT);
+			board.placePiece(rook, targetT);
+			rook.increaseMoveCount();
+		}
+		
+		// Implementando movimento da peca Torre no Roque Grande
+		if (p instanceof King && target.getColumn() == source.getColumn() - 2) { //Se peca que esta sendo movimentada for Rei e e posicao destino for igual posicao origem - 2, movimentar a peca Torre para posicao do movimento Roque Grande
+			Position sourceT = new Position(source.getRow(), source.getColumn() - 4);
+			Position targetT = new Position(source.getRow(), source.getColumn() - 1);
+			ChessPiece rook = (ChessPiece)board.removePiece(sourceT);
+			board.placePiece(rook, targetT);
+			rook.increaseMoveCount();
+		}
+		
 		return capturedPiece;
 	}
 	
@@ -118,6 +136,24 @@ public class ChessMatch {
 			board.placePiece(capturedPiece, target);
 			capturedPieces.remove(capturedPiece);
 			piecesOnTheBoard.add(capturedPiece);
+		}
+		
+		// Implementando movimento da peca Torre no Roque Pequeno
+		if (p instanceof King && target.getColumn() == source.getColumn() + 2) { //Se peca que esta sendo movimentada for Rei e e posicao destino for igual posicao origem + 2, movimentar a peca Torre para posicao do movimento Roque Pequeno
+			Position sourceT = new Position(source.getRow(), source.getColumn() + 3);
+			Position targetT = new Position(source.getRow(), source.getColumn() + 1);
+			ChessPiece rook = (ChessPiece)board.removePiece(targetT);
+			board.placePiece(rook, sourceT);
+			rook.decreaseMoveCount();
+		}
+		
+		// Implementando movimento da peca Torre no Roque Grande
+		if (p instanceof King && target.getColumn() == source.getColumn() - 2) { //Se peca que esta sendo movimentada for Rei e e posicao destino for igual posicao origem - 2, movimentar a peca Torre para posicao do movimento Roque Grande
+			Position sourceT = new Position(source.getRow(), source.getColumn() - 4);
+			Position targetT = new Position(source.getRow(), source.getColumn() - 1);
+			ChessPiece rook = (ChessPiece)board.removePiece(targetT);
+			board.placePiece(rook, sourceT);
+			rook.decreaseMoveCount();
 		}
 	}
 	
@@ -214,7 +250,7 @@ public class ChessMatch {
         placeNewPiece('b', 1, new Knight(board, Color.WHITE));
         placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('d', 1, new Queen(board, Color.WHITE));
-        placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('e', 1, new King(board, Color.WHITE, this));
         placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('g', 1, new Knight(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
@@ -231,7 +267,7 @@ public class ChessMatch {
         placeNewPiece('b', 8, new Knight(board, Color.BLACK));
         placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('d', 8, new Queen(board, Color.BLACK));
-        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 8, new King(board, Color.BLACK, this));
         placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('g', 8, new Knight(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -2,14 +2,18 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 //Peca Rei
 public class King extends ChessPiece{
 
-	public King(Board board, Color color) {
+	private ChessMatch chessMatch; 
+	
+	public King(Board board, Color color, ChessMatch chessMatch) { //Adicionando argumento chessMatch para que a classe Rei acesse a partida (Implementacao jogada Roque)
 		super(board, color);
+		this.chessMatch = chessMatch;
 	}
 	
 	@Override
@@ -22,6 +26,11 @@ public class King extends ChessPiece{
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p == null || p.getColor() != getColor();
 	}
+	
+	private boolean testRookCastling(Position position) { //Metodo para testar se a Torre esta apta para jogada Roque
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0; //Retorna true se posicao nao for nula, se peca da posicao for Torre, se a cor da  peca for a cor da jogada atual e se movimento de partida for zero
+	}	
 	
 	//Movimentos para Rei: Uma casa em qualquer direcao a partir da propria posicao desde que metodo acima retorne verdadeiro e posicao exista
 	@Override
@@ -76,6 +85,29 @@ public class King extends ChessPiece{
 		p.setValue(position.getRow() + 1, position.getColumn() + 1);
 		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Movimento especial Roque: Só podera executar se as pecas Rei e Torre nao foram movimentadas, partida nao estiver em check e casas dos lados estiverem vazias
+		if (getMoveCount() == 0 && !chessMatch.getCheck()) { 
+			//Testando Roque Pequeno (Lado direito)
+			Position posT1 = new Position(position.getRow(), position.getColumn() + 3); //Verificando a terceira posicao a direita depois do Rei
+			if (testRookCastling(posT1)) { // Se posicao for Torre e estiver apta ao movimento Roque, verificar se as duas posicoes ao lado esquerdo estao vazias
+				Position p1 = new Position(position.getRow(), position.getColumn() + 1);
+				Position p2 = new Position(position.getRow(), position.getColumn() + 2);
+				if (getBoard().piece(p1) == null && getBoard().piece(p2) == null) {//Se as posicoes estiverem vazias, retornar para matriz de movimentos possiveis verdadeiro (Apto a jogada Roque pequeno)
+					mat[position.getRow()][position.getColumn() + 2] = true;
+				}
+			}
+			//Testando Roque Grande (Lado esquerdo)
+			Position posT2 = new Position(position.getRow(), position.getColumn() - 4); //Verificando a quarrta posicao a esquerda depois do Rei
+			if (testRookCastling(posT2)) { // Se posicao for Torre e estiver apta ao movimento Roque, verificar se as tres posicoes ao lado direito estao vazias
+				Position p1 = new Position(position.getRow(), position.getColumn() - 1);
+				Position p2 = new Position(position.getRow(), position.getColumn() - 2);
+				Position p3 = new Position(position.getRow(), position.getColumn() - 3);
+				if (getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null) {//Se as posicoes estiverem vazias, retornar para matriz de movimentos possiveis verdadeiro (Apto a jogada Roque Grande)
+					mat[position.getRow()][position.getColumn() - 2] = true;
+				}
+			}	
 		}
 		
 		return mat;
